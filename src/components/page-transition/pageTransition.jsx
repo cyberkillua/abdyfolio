6e89e4c07588b2ef1c3f265eba1e9762d034fd00
alimi abdullahi <alimiabdullahi93@gmail.com>
2024-06-01 11:54:19 +0100

    fixed transition on loader

--- src/components/page-transition/pageTransition.jsx
+++ src/components/page-transition/pageTransition.jsx
@@ -8,8 +8,8 @@ const PageTransition = (Component) => {
         return (
             <>
                 <Component />
-                <motion.div className="slide_in" initial={{ scaleY: 0 }} animate={{ scaleY: 0 }} exit={{ scaleY: 1 }} transition={{ duration: 1, ease: [0.76, 0, 0.24, 1] }} />
-                <motion.div className="slide_out" initial={{ scaleY: 1 }} animate={{ scaleY: 0 }} exit={{ scaleY: 0 }} transition={{ duration: 1,  ease: [0.76, 0, 0.24, 1] }}/>
+                <motion.div className="slide_in" initial={{ scaleY: 0 }} animate={{ scaleY: 0 }} exit={{ scaleY: 1 }} transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }} />
+                <motion.div className="slide_out" initial={{ scaleY: 1 }} animate={{ scaleY: 0 }} exit={{ scaleY: 0 }} transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }} />
             </>
         );
     };
